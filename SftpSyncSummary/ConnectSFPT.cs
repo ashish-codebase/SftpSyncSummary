0fdd4caf04893687ac12b7ef91d3a857c11f54a8
Ashish Masih <masih.ashish@gmail.com>
2022-09-15 12:09:26 -0500

    etc

--- SftpSyncSummary/ConnectSFPT.cs
+++ SftpSyncSummary/ConnectSFPT.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using WinSCP;
 
 namespace SftpSyncSummary
 {
@@ -18,17 +20,123 @@ namespace SftpSyncSummary
             public string HPRCC_SummaryPath { get; set; }
         }
 
-        public List<SFTP_Parameter> ConnectionList { get; set; }
-
-        public ConnectSFPT(string CSV_Path)
+        public ConnectSFPT()
         {
-            ConnectionList = GetCSVFile(CSV_Path);
+
+        }
+        public List<SFTP_Parameter> GetParameters(string CSV_Path)
+        {
+            return GetCSVFile(CSV_Path);
         }
 
         private List<SFTP_Parameter> GetCSVFile(string CSV_Path)
         {
             return default;
         }
+
+        public void RunSingleSync(SFTP_Parameter sFTP_Parameter)
+        {
+            bool result = GetSummaryFile(sFTP_Parameter);
+            if (result)
+            {
+                UploadSummaryFile(sFTP_Parameter);
+            }
+            else
+            {
+                Console.WriteLine("File not downloaded");
+            }
+        }
+
+        private bool GetSummaryFile(SFTP_Parameter sFTP_Parameter)
+        {
+            string local_summary_text;
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = sFTP_Parameter.EC_IpAddress,
+                    UserName = sFTP_Parameter.EC_UserName,
+                    Password = sFTP_Parameter.EC_Password,
+                    PortNumber = sFTP_Parameter.EC_Port,
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    var directoryInfo = session.ListDirectory("/home/licor/data/summaries/");
+                    transferResult =
+                        session.PutFiles(@"d:\toupload\*", "/home/user/", false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            return true;
+        }
+        private void UploadSummaryFile(SFTP_Parameter sFTP_Parameter)
+        {
+            string local_summary_text;
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = sFTP_Parameter.HPRCC_IpAddress,
+                    UserName = sFTP_Parameter.HPRCC_UserName,
+                    Password = sFTP_Parameter.HPRCC_Password,
+                    PortNumber = sFTP_Parameter.HPRCC_Port,
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    var directoryInfo = session.ListDirectory("/home/licor/data/summaries/");
+                    transferResult =
+                        session.PutFiles(@"d:\toupload\*", "/home/user/", false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
     }
 
 }
--- SftpSyncSummary/Program.cs
+++ SftpSyncSummary/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using WinSCP;
+﻿using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SftpSyncSummary
 {
@@ -10,61 +7,14 @@ namespace SftpSyncSummary
     {
         private static void Main(string[] args)
         {
-            var Connections = new ConnectSFPT("");
-            List<ConnectSFPT.SFTP_Parameter> connections = Connections.ConnectionList;
-            foreach (var connection in connections)
+            ConnectSFPT connectSFPT = new ConnectSFPT();
+
+            List<ConnectSFPT.SFTP_Parameter> ConnectionList = connectSFPT.GetParameters("csv.csv");
+            foreach (var connection in ConnectionList)
             {
-                Thread t = new Thread(()=>RunSingleSync(connection));
+                Thread t = new Thread(() => connectSFPT.RunSingleSync(connection));
                 t.Start();
             }
-
-            void RunSingleSync(ConnectSFPT.SFTP_Parameter sFTP_Parameter)
-            {
-                try
-                {
-                    // Setup session options
-                    SessionOptions sessionOptions = new SessionOptions
-                    {
-                        Protocol = Protocol.Sftp,
-                        HostName = sFTP_Parameter.EC_IpAddress,
-                        UserName = sFTP_Parameter.EC_UserName,
-                        Password = sFTP_Parameter.EC_Password,
-                        PortNumber = sFTP_Parameter.EC_Port,
-                        RootPath = sFTP_Parameter.EC_SummaryPath
-                    };
-
-                    using (Session session = new Session())
-                    {
-                        // Connect
-                        session.Open(sessionOptions);
-
-                        // Upload files
-                        TransferOptions transferOptions = new TransferOptions();
-                        transferOptions.TransferMode = TransferMode.Binary;
-
-                        TransferOperationResult transferResult;
-                        transferResult =
-                            session.PutFiles(@"d:\toupload\*", "/home/user/", false, transferOptions);
-
-                        // Throw on any error
-                        transferResult.Check();
-
-                        // Print results
-                        foreach (TransferEventArgs transfer in transferResult.Transfers)
-                        {
-                            Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Error: {e.Message}");
-                }
-            }
         }
-
-
-
     }
-
 }
